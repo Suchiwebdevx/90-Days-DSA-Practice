30971a09bb1f8cf779277ce835520462fb2c42fe
Suchi <suchiupadhayay442@gmail.com>
2026-08-04 21:54:29 +0530

    Add files via upload

---
+++ min.java
@@ -0,0 +1,20 @@
+// Find minimum in the array using linear Search
+public class min {
+	public static void main(String args[]) {
+		int[]arr = {18,12,7,3,14,28};
+		int ans=LinearSearch(arr);
+		System.out.println(ans);
+	}
+	static int LinearSearch(int[] arr) {
+		int ans=arr[0];
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]<ans) {
+				ans=arr[i];
+			}
+		}
+		return ans;
+	}
+
+}
+
+
