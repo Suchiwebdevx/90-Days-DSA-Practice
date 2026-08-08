998ad65fc300211e72e914cd595c0c35d3c00e4a
Suchi <suchiupadhayay442@gmail.com>
2026-08-08 22:55:26 +0530

    Add files via upload

---
+++ CeilingNumber.java
@@ -0,0 +1,38 @@
+package com.BinarySearch;
+
+public class CeilingNumber {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int target = 15;
+
+        int ans = ceiling(arr, target);
+
+        System.out.println(ans);
+    }
+
+    static int ceiling(int[] arr, int target) {
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return arr[mid];
+            }
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        // start points to the ceiling
+        return arr[start];
+    }
+}
