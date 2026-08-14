f1407519f4d7ab92bf5df81dc77a5fdb490adeeb
Suchi <suchiupadhayay442@gmail.com>
2026-08-14 21:11:23 +0530

    Add files via upload

---
+++ LargestValueinArray.java
@@ -0,0 +1,25 @@
+//Find the largest element in an array.
+
+package com.Arraypractice;
+
+public class LargestValueinArray {
+	public static void main(String[] args) {
+
+        int[] arr = {10, 5, 25, 8, 15};
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+        	
+            if (arr[i] > max) {
+            	
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Largest element: " + max);
+    }
+	
+
+
+}
