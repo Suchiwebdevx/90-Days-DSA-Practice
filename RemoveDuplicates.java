b93ee3c1a3822505ed304006456525669dc63971
Suchi <suchiupadhayay442@gmail.com>
2026-08-15 20:44:45 +0530

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,25 @@
+//Remove Duplicates from a Sorted Array
+
+package com.Arraypractice;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 1, 2, 2, 3, 4, 4};
+
+        int j = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+
+        for (int i = 0; i <= j; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
