fdcd78f91853f0507438e2892718c12bd49c2b59
Suchi <suchiupadhayay442@gmail.com>
2026-08-15 20:44:30 +0530

    Add files via upload

---
+++ MoveZeros.java
@@ -0,0 +1,29 @@
+//Move All Zeros to the End
+
+package com.Arraypractice;
+
+public class MoveZeros {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 0, 2, 0, 3, 0};
+
+        int j = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] != 0) {
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+                j++;
+            }
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
