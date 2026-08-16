aa3611a474d83f1cb760b387d09e501b27a4d478
Suchi <suchiupadhayay442@gmail.com>
2026-08-16 20:22:48 +0530

    Add files via upload

---
+++ RotateArray.java
@@ -0,0 +1,23 @@
+//Rotate Array by One Position
+
+package com.Arraypractice;
+
+import java.util.Arrays;
+
+public class RotateArray {
+	public static void main(String args[]) {
+		int[] arr = {1, 2, 3, 4, 5};
+
+		int last = arr[arr.length - 1];
+
+		for (int i = arr.length - 1; i > 0; i--) {
+			
+		    arr[i] = arr[i - 1];
+		}
+
+		arr[0] = last;
+		
+		System.out.println(Arrays.toString(arr));
+		}
+
+}
