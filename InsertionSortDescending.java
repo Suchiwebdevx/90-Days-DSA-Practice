f966ea522ce82d1032f972a0a8a93692f080b59a
Suchi <suchiupadhayay442@gmail.com>
2026-08-17 20:27:01 +0530

    Add files via upload

---
+++ InsertionSortDescending.java
@@ -0,0 +1,31 @@
+//Sort an array in descending order
+
+package InsertionSort;
+
+import java.util.Arrays;
+
+public class InsertionSortDescending {
+	
+    public static void main(String[] args) {
+
+        int[] arr = {5, 2, 4, 1, 3};
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+            
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] < key) {
+            	
+                arr[j + 1] = arr[j];
+                
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
