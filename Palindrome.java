9ffe67e3663c8d03ff8188ade978b73ceb88bcda
Suchi <suchiupadhayay442@gmail.com>
2026-08-19 20:28:09 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,39 @@
+//Check Whether a String is Palindrome
+
+package com.DSA;
+
+public class Palindrome {
+	
+    public static void main(String[] args) {
+
+        String str = "madam";
+
+        int start = 0;
+        
+        int end = str.length() - 1;
+
+        boolean palindrome = true;
+
+        while (start < end) {
+
+            if (str.charAt(start) != str.charAt(end)) {
+            	
+                palindrome = false;
+                
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (palindrome) {
+        	
+            System.out.println("Palindrome");
+            
+        } else {
+        	
+            System.out.println("Not Palindrome");
+        }
+    }
+}
