e087732abac35f6d4d14e8394072702e7823b3b1
Suchi <suchiupadhayay442@gmail.com>
2026-08-19 20:44:44 +0530

    Add files via upload

--- FindDuplicate.java
+++ FindDuplicate.java
@@ -1,30 +1,35 @@
-//Find Duplicate Elements
+//Find the Duplicate Number
 
-package com.Arraypractice;
+package com.DSA;
 
 public class FindDuplicate {
-
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 3, 2, 4, 1};
+        int[] arr = {1, 3, 4, 2, 2};
 
-        for (int i = 0; i < arr.length; i++) {
-        	
+        int i = 0;
 
-            for (int j = i + 1; j < arr.length; j++) {
-            	
+        while (i < arr.length) {
 
-                if (arr[i] == arr[j]) {
-                	
-                    System.out.println(arr[i]);
-                    
-                    break;
-                }
-                
+            int correctIndex = arr[i] - 1;
+
+            if (arr[i] != arr[correctIndex]) {
+
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+
+            } else {
+                i++;
             }
-            
         }
-        
+
+        for (int index = 0; index < arr.length; index++) {
+
+            if (arr[index] != index + 1) {
+                System.out.println("Duplicate number: " + arr[index]);
+                break;
+            }
+        }
     }
-    
 }
