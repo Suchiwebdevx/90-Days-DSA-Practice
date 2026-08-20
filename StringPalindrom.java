cbd872fcc61760acb4509935079e53345ecd391e
Suchi <suchiupadhayay442@gmail.com>
2026-08-20 20:03:36 +0530

    Add files via upload

---
+++ StringPalindrom.java
@@ -0,0 +1,33 @@
+//Check if a String is Palindrome
+
+package com.main;
+
+public class StringPalindrom {
+    public static void main(String[] args) {
+
+        String str = "madam";
+        
+        boolean isPalindrome = true;
+
+        int start = 0;
+        
+        int end = str.length() - 1;
+
+        while (start < end) {
+
+            if (str.charAt(start) != str.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
