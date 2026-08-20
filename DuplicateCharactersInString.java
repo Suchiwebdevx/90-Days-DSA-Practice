07eced170ab7a33caaab902a8fd20a2bdf7cdff4
Suchi <suchiupadhayay442@gmail.com>
2026-08-20 20:02:49 +0530

    Add files via upload

---
+++ DuplicateCharactersInString.java
@@ -0,0 +1,29 @@
+//Find Duplicate Characters in a String
+
+package com.main;
+
+public class DuplicateCharactersInString {
+	
+    public static void main(String[] args) {
+
+        String str = "programming";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            for (int j = i + 1; j < str.length(); j++) {
+
+                if (str.charAt(i) == str.charAt(j)) {
+                	
+                    System.out.println(str.charAt(i));
+                    
+                    break;
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+}
