aa3ae2f5ca52661e8ea774418103891e0b79d596
Suchi <suchiupadhayay442@gmail.com>
2026-08-26 20:35:18 +0530

    Add files via upload

---
+++ ReverseTriangle.java
@@ -0,0 +1,24 @@
+package com.Pattern;
+
+public class ReverseTriangle {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+
+            // spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
