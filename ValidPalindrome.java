a0bedc2d150b7041278a8a7aaf57dfd5aff8e75d
Suchi <suchiupadhayay442@gmail.com>
2026-08-27 20:47:20 +0530

    Add files via upload

---
+++ ValidPalindrome.java
@@ -0,0 +1,28 @@
+package com.Pattern;
+
+public class ValidPalindrome {
+
+    public static void main(String[] args) {
+
+        String str = "madam";
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        boolean palindrome = true;
+
+        while (start < end) {
+
+            if (str.charAt(start) != str.charAt(end)) {
+
+                palindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        System.out.println("Palindrome: " + palindrome);
+    }
+}
