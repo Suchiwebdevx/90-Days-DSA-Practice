84b0ab1dbbd6722916c41cafeb7a60322d07ae2e
Suchi <suchiupadhayay442@gmail.com>
2026-08-29 20:06:59 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+//Factorial of the number
+
+package com.BinarySearch;
+
+public class Factorial {
+	
+	public static void main(String[] args) {
+		
+		int n=8;
+		
+		int factorial=1;
+		
+		for(int i =1;i<=n;i++) {
+			factorial=factorial *i;
+		}
+		
+		System.out.println("Factorial " + factorial)	;	
+	}
+
+}
