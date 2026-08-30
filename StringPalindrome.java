1f03b785b057104ddc4213b77ea1eef8f6db22c8
Suchi <suchiupadhayay442@gmail.com>
2026-08-30 20:39:14 +0530

    Add files via upload

---
+++ StringPalindrome.java
@@ -0,0 +1,31 @@
+//String is palindrome or not
+
+package com.DSA;
+
+public class StringPalindrome {
+	
+	public static void main(String args[]) {
+	
+
+String str = "madam";
+
+String reverse = "";
+
+for (int i = str.length() - 1; i >= 0; i--) {
+	
+    reverse = reverse + str.charAt(i);
+}
+
+if (str.equals(reverse)) {
+	
+    System.out.println("Palindrome");
+    
+} else {
+	
+    System.out.println("Not Palindrome");
+    
+}
+
+}
+	
+}
