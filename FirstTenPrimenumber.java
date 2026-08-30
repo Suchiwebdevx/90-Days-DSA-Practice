83c4588cabeff04ac026ec659233351e6adc54da
Suchi <suchiupadhayay442@gmail.com>
2026-08-30 20:29:33 +0530

    Add files via upload

---
+++ FirstTenPrimenumber.java
@@ -0,0 +1,19 @@
+//Print numbers from 1 to 10
+
+package com.DSA;
+
+public class FirstTenPrimenumber {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <=10;i++) {
+			
+			if(i % 2 == 0) {
+				
+           System.out.println(i);
+           
+			}
+		}
+	}
+
+}
