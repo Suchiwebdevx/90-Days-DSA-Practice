f5ba9b854a4c5bb872651719bc04f4d35e2124cb
Suchi <suchiupadhayay442@gmail.com>
2026-08-30 20:30:09 +0530

    Add files via upload

---
+++ LargestTwo.java
@@ -0,0 +1,18 @@
+//Find the largest two
+
+package com.DSA;
+
+public class LargestTwo {
+	
+    public static void main(String[] args) {
+
+        int a = 25;
+        int b = 40;
+
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+}
