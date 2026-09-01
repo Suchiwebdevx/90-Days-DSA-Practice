6e2be27167855fbbc84e0707ce9c69988324748f
Suchi <suchiupadhayay442@gmail.com>
2026-09-01 20:53:11 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,23 @@
+//write program to print multiplication table from 2 to 20
+
+package com.Pattern;
+
+public class MultiplicationTable {
+	
+	public static void main(String args[]) {
+		
+		for(int i = 2; i <= 20 ; i++) {
+			
+			System.out.println("Table of " + i);
+			
+			for(int j = 1; j <=10 ; j++) {
+				
+				System.out.println(i + " x " + j + " = " + (i*j));
+			}
+			
+			System.out.println();
+			
+		}
+	}
+
+}
