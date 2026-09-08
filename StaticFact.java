5e7792598e3bf5c05d23633dddecc2f411b3390c
Suchi <suchiupadhayay442@gmail.com>
2026-09-08 21:57:46 +0530

    Add files via upload

---
+++ StaticFact.java
@@ -0,0 +1,19 @@
+package OOps;
+
+public class StaticFact {
+
+    static void factorial(int n) {
+
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial = " + fact);
+    }
+
+    public static void main(String[] args) {
+        factorial(5);
+    }
+}
