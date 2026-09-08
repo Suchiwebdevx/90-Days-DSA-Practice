202f5a039b74a8f178b8318a48af2a3331ce5847
Suchi <suchiupadhayay442@gmail.com>
2026-09-08 21:13:54 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,16 @@
+//Print numbers from 10 to 1
+
+package OOps;
+
+public class Loops {
+	public static void main(String args[]) {
+		
+		System.out.println("The NUmbers are:");
+		
+		for(int i =10 ; i>=1;i--) {
+			
+			System.out.println(i);		}
+	}
+	
+
+}
