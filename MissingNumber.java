c417c864abed96ae20f5be33c13a78ccfe088305
Suchi <suchiupadhayay442@gmail.com>
2026-09-10 21:12:06 +0530

    Add files via upload

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,39 +1,28 @@
-//Find the Missing Number
-
-package com.DSA;
+package OOps;
 
 public class MissingNumber {
-	
+
+    static int findMissing(int[] arr, int n) {
+
+        int totalSum = n * (n + 1) / 2;
+
+        int arraySum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arraySum = arraySum + arr[i];
+        }
+
+        return totalSum - arraySum;
+    }
+
     public static void main(String[] args) {
 
-        int[] arr = {4, 0, 2, 1};
+        int[] arr = {1, 2, 4, 5};
 
-        int i = 0;
+        int n = 5;
 
-        while (i < arr.length) {
+        int result = findMissing(arr, n);
 
-            int correctIndex = arr[i];
-
-            if (arr[i] < arr.length &&
-                arr[i] != arr[correctIndex]) {
-
-                int temp = arr[i];
-                arr[i] = arr[correctIndex];
-                arr[correctIndex] = temp;
-
-            } else {
-                i++;
-            }
-        }
-
-        for (int index = 0; index < arr.length; index++) {
-
-            if (arr[index] != index) {
-                System.out.println("Missing number: " + index);
-                return;
-            }
-        }
-
-        System.out.println("Missing number: " + arr.length);
+        System.out.println("Missing number = " + result);
     }
 }
